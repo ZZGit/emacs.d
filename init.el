6d110dd91788f71fd8a5f050997103e1d968104d
strongfish <569721086@qq.com>
2021-03-22 00:16:16 +0800

    add undo-tree

--- init.el
+++ init.el
@@ -34,17 +34,5 @@
 ;; web开发
 (require 'init-web)
 
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(web-beautify js2-mode json-mode emmet-mode web-mode spacemacs-theme counsel magit ivy use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+;; undo
+(require 'init-undotree)
---
+++ lisp/init-undotree.el
@@ -0,0 +1,8 @@
+;; #init-undotree.el
+;; https://www.dr-qubit.org/undo-tree/undo-tree.txt
+
+(use-package undo-tree
+  :ensure t
+  :init (global-undo-tree-mode))
+
+(provide 'init-undotree)
